c4f6ceaffedf105ad55677e308788d7692d943f5
Evryon <fireden70@gmail.com>
2023-07-09 20:48:37 +0200

    did some setup after working out and eating and cooking and watching some youtube no i did not take 3 hours to write 10 lines i feel the need to specify this because someone might just go here and see "hmmm 3 hours since the start comming" NO ok? ok? no, alright i wrote 10 lines of code this is very productive hmm yes yes now im gonna shower and cook some chicken baybeee gotta get that protein 💪

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,33 @@
+#![feature(try_blocks)]
+
+use colour::{cyan_ln, magenta_ln, red_ln};
+use std::env;
+use std::fs::File;
+use std::io::Write;
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    for i in &args {
+        println!("{}", i);
+        // Match flags here
+    }
+    if let Ok(_) = std::fs::create_dir("writhe") {
+        log("writhe directory created");
+    } else {
+        log("writhe directory already exists");
+    }
+
+    File::create(format!("writhe/{}", args[1].replace("rs", "py")))
+        .unwrap()
+        .write_all(format!("{}\n", funny()).as_bytes())
+        .unwrap();
+
+}
+fn funny() -> String {
+    String::from("funny")
+}
+
+pub fn log(msg: &str) {
+    //todo: put these on a log file in /writhe later i should probably put this fn in lib.rs
+    magenta_ln!("{}", msg);
 }
