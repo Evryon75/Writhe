ae59b42af9660d01bba97da04fb54315cdd8ca1d
Evryon <fireden70@gmail.com>
2023-07-10 22:01:46 +0200

    TIME SLIPS AWAY BURN OUT DONT FADE DANCE ON MY GRAVE, I WILL NEVER BE THEM, I WILL NEVER BE THEM, I WILL NEVER BE THEM, I WILL NEVER BE THEM, I WILL NEVER BE THEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEM YEAAAAHAHA NO, I WILL NEVER BE THEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEM I WILL ALWAYS BE A CODE MISTAAAAAKE

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,61 @@
-#![feature(try_blocks)]
+mod lexer;
 
-use colour::{cyan_ln, magenta_ln, red_ln};
+use colour::{cyan_ln, green_ln, magenta_ln, red_ln, white_ln};
 use std::env;
 use std::fs::File;
 use std::io::Write;
-use std::time::SystemTime;
 
 fn main() {
-    log("starting writhe");
+    if let Ok(_) = std::fs::create_dir("writhe") {
+        std::fs::create_dir("writhe/logs").unwrap();
+    }
+    let mut runtime_logs = File::create(format!("writhe/logs/runtime_log.txt")).unwrap();
+    let mut log = |msg: &str, pri: Option<PrintColors>| {
+        // Declared as a closure so that we can use a variable to reference the log file
+        // instead of having to create it every time we log something, premature optimization? maybe
+        // This is probably a terrible idea and i can see myself refactoring for HOURS but whatever
+        let fm = format!(
+            "[{}] >> {}\n",
+            chrono::offset::Local::now().to_string(),
+            msg
+        );
+        runtime_logs.write_all(fm.as_bytes()).unwrap();
+        if let Some(color) = pri {
+            match color {
+                PrintColors::Red => red_ln!("{}", msg),
+                PrintColors::Green => green_ln!("{}", msg),
+                PrintColors::Cyan => cyan_ln!("{}", msg),
+                PrintColors::Magenta => magenta_ln!("{}", msg),
+                PrintColors::White => white_ln!("{}", msg),
+            }
+        }
+    };
 
     let args: Vec<String> = env::args().collect();
-    log(format!("args: {:#?}", args).as_str());
     for i in &args {
         // Match flags here
     }
 
-    if let Ok(_) = std::fs::create_dir("writhe") {
-        log("writhe directory created");
-    } else {
-        log("writhe directory already exists");
+    log(format!("args: {:#?}", args).as_str(), None);
+    log("starting writhe", Some(PrintColors::Green));
+
+    let teething: Result<&str, &str> = Ok("hi");
+    match teething {
+        Ok(res) => File::create(format!("writhe/{}", args[1].replace("rs", "py")))
+            .unwrap()
+            .write_all(format!("{}\n", res).as_bytes())
+            .unwrap(),
+        Err(res) => File::create(format!("writhe/logs/attempt_log.txt"))
+            .unwrap()
+            .write_all(res.as_bytes())
+            .unwrap(),
     }
-
-    File::create(format!("writhe/{}", args[1].replace("rs", "py")))
-        .unwrap()
-        .write_all(format!("{}\n", "**Resulting python code goes here**").as_bytes())
-        .unwrap();
-
 }
 
-pub fn log(msg: &str) {
-    //todo: put these on a log file in /writhe later i should probably put this fn in lib.rs
-    magenta_ln!("{:?} >> {}\n", chrono::offset::Local::now(), msg);
+enum PrintColors {
+    Red,
+    Green,
+    Cyan,
+    Magenta,
+    White,
 }
