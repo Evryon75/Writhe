1f8b9e956861cdcd331f8279eccf48c7a2da2d53
Evryon <fireden70@gmail.com>
2023-07-12 22:38:16 +0200

    bout to hit some chest

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,34 @@
-pub fn lex() {
-    // Call this from like idk the ast thing?
+use colour::*;
+
+pub fn lex(input: Vec<String>) {
+    magenta_ln!("{:#?}", input);
+}
+
+fn tokenize(word: String) {
+
+}
+
+fn advanced(seq: String) {
+
+}
+
+enum Token {
+    Identifier(String),
+    Fn,
+    Let,
+    Use,
+    Mut,
+    For,
+    In,
+    Match,
+    As,
+    Enum,
+    Colon,
+    Semicolon,
+    RoundLeft,
+    RoundRight,
+    SquareLeft,
+    SquareRight,
+    CurlyLeft,
+    CurlyRight,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod lexer;
 
+use crate::lexer::lex;
 use std::env;
 use std::fs::File;
-use std::io::Write;
-
+use std::io::{Read, Write};
 use writhe::{init, log};
 
 fn main() {
@@ -14,6 +14,21 @@ fn main() {
         // Match flags here
     }
     log(format!("args: {:#?}", args).as_str(), None);
+    let mut contents = String::new();
+    File::open(&args[1])
+        .unwrap()
+        .read_to_string(&mut contents)
+        .unwrap();
+    println!(
+        "LEX FN PRINT:{:#?}",
+        lex({
+            let mut collection: Vec<String> = vec![];
+            contents.replace("\r\n", " ").split(" ").for_each(|e| {
+                collection.push(e.to_owned());
+            });
+            collection
+        })
+    );
 
     //try this let (file, bytes) = match ...; line before the File::create line, and then it would be File::create(file).unwrap().write_all(bytes).unwrap()
     match Ok("hi") as Result<&str, &str> {
