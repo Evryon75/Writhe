f206d67b872f01da8991071484af23f53c24622f
Evryon <fireden70@gmail.com>
2023-07-15 14:59:31 +0200

    ok ok we got some stuff goin on i think

--- src/lexer.rs
+++ src/lexer.rs
@@ -2,25 +2,38 @@ use colour::*;
 use writhe::Token;
 
 pub fn lex(input_raw: String) {
-    //dont add spaces in between stuff and remake everything again god fucking damnit bro
     let mut tokens: Vec<Token> = vec![];
     let mut state = LexerState::Free;
+    let mut prev_state = LexerState::Free;
     for line in input_raw.lines().filter(|s| !s.trim().starts_with("//")) {
-
         let chars: Vec<char> = line.chars().collect();
+
         const IDENT_CHARS: &str = "_qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
         const IDENT_DIGITS: &str = "1234567890";
+        const DIGITS: &str = "1234567890._";
+
         let mut build = String::new();
         let mut i = 0;
         while i < chars.len() {
             let char = chars[i];
 
             if let Some(tok) = match state {
+                LexerState::Number => {
+                    let result = None;
+
+                    if !DIGITS.contains(chars[i + 1]) {
+                        state = LexerState::Free;
+                        prev_state = LexerState::Number;
+                    }
+
+                    result
+                }
                 LexerState::String => {
                     let result = None;
 
                     if chars[i] == '\"' {
                         state = LexerState::Free;
+                        prev_state = LexerState::String;
                     }
 
                     result
@@ -28,21 +41,49 @@ pub fn lex(input_raw: String) {
                 LexerState::Comment => {
                     if chars[i] == '/' && chars[i - 1] == '*' {
                         state = LexerState::Free;
+                        prev_state = LexerState::Comment;
                     }
                     None
                 }
                 LexerState::Ident => {
                     let result = None;
+
+                    if !IDENT_CHARS.contains(chars[i + 1]) && !IDENT_DIGITS.contains(chars[i + 1]) {
+                        state = LexerState::Free;
+                        prev_state = LexerState::Ident;
+                    }
+
+                    build.push(char);
+
                     result
                 }
                 LexerState::Free => {
                     let mut result: Option<Token> = None;
+
+                    if !build.is_empty() {
+                        result = Some(match prev_state {
+                            LexerState::Number => Token::NumericLiteral(build.parse::<usize>().unwrap()),
+                            LexerState::String => Token::StringLiteral(build.to_string()),
+                            LexerState::Ident => Token::Identifier(build.to_string()),
+                            LexerState::Comment => unreachable!(),
+                            LexerState::Free => unreachable!()
+                        })
+                    }
+
                     if chars[i] == '/' && chars[i + 1] == '*' {
                         state = LexerState::Comment;
+                        prev_state = LexerState::Free;
                     } else if chars[i] == '\"' {
                         state = LexerState::String;
+                        prev_state = LexerState::Free;
                     } else if IDENT_CHARS.contains(char) {
                         state = LexerState::Ident;
+                        prev_state = LexerState::Free;
+
+                        build.push(char);
+                    } else if IDENT_DIGITS.contains(chars[i]) {
+                        state = LexerState::Number;
+                        prev_state = LexerState::Free;
                         build.push(char);
                     } else {
                         result = tokenize(char.to_string().as_str());
@@ -53,7 +94,8 @@ pub fn lex(input_raw: String) {
                 tokens.push(tok);
             }
 
-            white_ln!("{}", char);
+            white_ln!("{}, {:?}", char, state);
+            build.clear();
             i += 1;
         }
     }
@@ -62,6 +104,7 @@ pub fn lex(input_raw: String) {
 }
 
 fn tokenize(word: &str) -> Option<Token> {
+    green_ln!("{:#?}", word);
     match word {
         "+" => Some(Token::Add),
         "-" => Some(Token::Subtract),
@@ -80,6 +123,7 @@ fn tokenize(word: &str) -> Option<Token> {
         "let" => Some(Token::Let),
         "use" => Some(Token::Use),
         "mut" => None,
+        "!" => None,
         "for" => Some(Token::For),
         "in" => Some(Token::In),
         "match" => Some(Token::Match),
@@ -106,7 +150,9 @@ fn thorough_tokenize(seq: &str) -> Option<Token> {
     Some(Token::Undefined)
 }
 
+#[derive(Debug)]
 enum LexerState {
+    Number,
     String,
     Comment,
     Ident,
